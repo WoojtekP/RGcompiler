1ff22c7d80304d0b6d66f66badccddb40c8539ea
WoojtekP <pawlik.wj@gmail.com>
2022-07-27 16:56:20 +0200

    fix DefaultMap to work correctly with const objects

--- defaultMap.hpp
+++ defaultMap.hpp
@@ -1,12 +1,14 @@
+#pragma once
+
 #include <map>
 
 
 template<class Key, class T>
 class DefaultMap : public std::map<Key, T>
 {
+public:
     using value_type = std::pair<const Key, T>;
 
-public:
     DefaultMap() = default;
 
     DefaultMap(const T& defaultValue, std::initializer_list<value_type> init)
@@ -21,6 +23,12 @@ public:
     T& operator[](const Key& key)
     {
         return std::map<Key, T>::insert(std::make_pair(key, defaultValue_)).first->second;
+    }
+
+    const T& operator[](const Key& key) const
+    {
+        const auto it = std::map<Key, T>::find(key);
+        return it == std::map<Key, T>::end() ? defaultValue_ : it->second;
     }
 
 private:

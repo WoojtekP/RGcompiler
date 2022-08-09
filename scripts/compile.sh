6e4fe25c70214fd7d6ac7dcd67105468f0663297
MSz <marek.esz@gmail.com>
2022-08-09 15:51:55 +0200

    Minor usage improvement

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-# usage: compile.sh [game]
+
+if [[ $# -ne 1 ]] ; then
+  echo "usage: compile.sh [game]"
+  exit 1
+fi
 
 set -e
 set -o pipefail
 
 game=$1
+echo "Compiling $1"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${DIR}
@@ -18,7 +23,6 @@ rm ${game}.json
 
 # generate cpp files
 ./build/rg2cpp ${game}-ast.json
-
 
 # format generated files
 clang-format -style="{BasedOnStyle: Google, IndentWidth: 4}" -i reasoner.hpp

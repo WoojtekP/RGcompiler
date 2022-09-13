d93f1415d8e542a80caa06511a7f854af512ac87
WoojtekP <pawlik.wj@gmail.com>
2022-09-13 23:52:08 +0200

    Fix printing MapValue

--- src/program/program.cpp
+++ src/program/program.cpp
@@ -61,7 +61,7 @@ std::string MapValue::toString(const std::shared_ptr<IType>& t, const TypeToSymb
             const int pos = symbolToValueMap.at(id);
             values[pos] = value->toString(functionType->destination, typeToSymbolToValueMap);
         }
-        std::string result = "{";
+        std::string result = "std::array{";
         for (const auto& value : values)
         {
             result += value + ",";

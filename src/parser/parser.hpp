82d16abd47f7a19be68b6928168be314e0c606cf
WoojtekP <pawlik.wj@gmail.com>
2023-02-24 00:22:02 +0100

    remove unused methods from Parser

--- src/parser/parser.hpp
+++ src/parser/parser.hpp
@@ -10,8 +10,6 @@
 #include <nlohmann/json.hpp>
 
 
-using TypeToSymbolToValueMap = std::map<std::string, std::map<std::string, int>>;
-
 class Parser
 {
 public:
@@ -20,9 +18,6 @@ public:
     nlohmann::json getVariables() const;
     nlohmann::json getConstants() const;
     nlohmann::json getEdges() const;
-    std::string getValue(const std::string& symbol) const;
-    const std::map<std::string, int>& getSymbolToValueMap() const;
-    const TypeToSymbolToValueMap& getTypeToSymbolToValueMap() const;
     std::vector<std::string> getDomain(const std::string& typeIdentifier) const;
     std::string getSourceType(const nlohmann::json& t) const;
     nlohmann::json getDestinationType(const nlohmann::json& t) const;

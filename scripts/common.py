d3dc32100af07882f98ce54ef42350fcb6b2fa78
WoojtekP <pawlik.wj@gmail.com>
2023-03-07 21:41:51 +0100

    Add --notmalizeTypes to default translate options

--- scripts/common.py
+++ scripts/common.py
@@ -21,7 +21,7 @@ class cfg:
   BUILD_TEST_DIR = 'build-test'
   RG_DIR = '../rg'
 
-  DEFAULT_TRANSLATE_OPTIONS = '--expandGeneratorNodes --compactSkipEdges --reuseFunctions'
+  DEFAULT_TRANSLATE_OPTIONS = '--expandGeneratorNodes --compactSkipEdges --reuseFunctions --normalizeTypes'
   DEFAULT_RG2CPP_OPTIONS = '--opt-conditions 3 --simple-path-compression 1 --move-compression 1'
 
   result = runCap('g++ --version')

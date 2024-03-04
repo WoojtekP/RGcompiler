0222fb87940713a9e18e7aaf5b611870860e591e
MSz <marek.esz@gmail.com>
2024-03-04 20:51:39 +0100

    Add --calculateTagIndexes

--- scripts/common.py
+++ scripts/common.py
@@ -21,8 +21,9 @@ class cfg:
   BUILD_TEST_DIR = 'build-test'
   RG_DIR = '../rg'
 
-  DEFAULT_TRANSLATE_OPTIONS = '--expandGeneratorNodes --compactSkipEdges --reuseFunctions --normalizeTypes --addExplicitCasts --calculateUniques'
-  DEFAULT_RG2CPP_OPTIONS = '--opt-conditions 3 --simple-path-compression 0 --no-cycle-detection 0 --print-function-names 0 --preserve-original-node-names 0 --verification 0 --disjoint 1'
+  DEFAULT_TRANSLATE_OPTIONS = '--expandGeneratorNodes --compactSkipEdges --reuseFunctions --normalizeTypes --addExplicitCasts'
+  DEFAULT_TRANSLATE_OPTIONS += ' --calculateUniques --calculateTagIndexes' # Auto-optimization
+  DEFAULT_RG2CPP_OPTIONS = '--opt-conditions 3 --simple-path 1'
 
   result = runCap('g++ --version')
   if 'clang' in decodeOutput(result.stdout):
@@ -30,7 +31,7 @@ class cfg:
     GCC_BENCHMARK_FLAGS = '-Wall -Wextra -std=c++17 -Ofast -flto -DNDEBUG'
   else:
     GCC_TEST_FLAGS = '-Wall -Wextra -std=c++17 -Ofast -flto=auto -march=native -ftracer'
-    GCC_BENCHMARK_FLAGS = '-Wall -Wextra -std=c++17 -Ofast -flto=auto  -march=native -ftracer -DNDEBUG'
+    GCC_BENCHMARK_FLAGS = '-Wall -Wextra -std=c++17 -Ofast -flto=auto  -march=native -ftracer -DNDEBUG -finline-limit=100'
 
 class util:
   RESET = "\033[0m"

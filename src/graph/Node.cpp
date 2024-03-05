b4771a365db50cc71e6d156995771784512d79d5
WoojtekP <pawlik.wj@gmail.com>
2024-03-05 14:44:56 +0100

    Fix handling pragma unique for nodes with generators

--- src/graph/Node.cpp
+++ src/graph/Node.cpp
@@ -65,6 +65,12 @@ std::string Node::getName() const
     return toString();
 }
 
+// TODO: it's a temporary solution: use the same name in all places
+std::string Node::getAlternativeName() const
+{
+    return binding_.has_value() ? name_ + binding_->toTagStringId() : name_;
+}
+
 const std::optional<Binding>& Node::getBinding() const
 {
     return binding_;

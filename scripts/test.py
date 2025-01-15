d772f28381bc1025a2707656c59ebc2ad4fa4d54
MSz <marek.esz@gmail.com>
2025-01-15 18:50:25 +0100

    More tests

--- scripts/test.py
+++ scripts/test.py
@@ -33,40 +33,31 @@ else:
 # (sims, avgDepth, [avgScore0,...], [perft0,perft1,...]
 
 tests = {}
-tests['ticTacToe.rg'] = (100000,7.63,[64.84,35.16], [1,9,72,504,3024,15120,54720]) # 148176 200448 127872
-tests['ticTacToe.rbg'] = tests['ticTacToe.rg']
 
-tests['breakthrough.rg'] = (10000,64.10,[50.92,49.08], [1,22,484,11132,256036,6182818]) # 149264638
-tests['breakthrough.hrg'] = tests['breakthrough.rg']
-tests['breakthrough.rbg'] = tests['breakthrough.rg']
+tests['repeatTest'] = (100,1.0,[100.0], [1,1,0])
+tests['repeatTestBig'] = (1000,1.0,[100.0], [1,2,0])
+tests['repeatTestHard'] = (1000,1.0,[12.5], [1,16,0])
+tests['simpleApplyTest0'] = (1000,1.0,[0.0], [1,2,0])
+tests['simpleApplyTest1'] = (1000,2.0,[0.0,50.0], [1,2,4,0])
+tests['simpleApplyTest2'] = (1000,2.0,[0.0,100.0], [1,2,2,0])
+tests['simpleApplyTest3'] = (1000,2.0,[0.0,75.0], [1,2,3,0])
+tests['simpleApplyTest4'] = (1000,2.0,[0.0,50.0], [1,1,3,0])
+tests['simpleApplyTest5'] = (1000,1.0,[50.0], [1,2,0])
+tests['simpleApplyTest6'] = (1000,2.0,[0.0,50.0], [1,3,5,0])
 
-tests['connect4.hrg'] = (10000,21.31,[55.72,44.28], [1,7,49,343,2401,16807]) # 117649 823536 5673234
+tests['amazons'] = (200,71.46,[50.10,49.90], [1,2176]) # 4307152
+tests['breakthrough'] = (10000,64.10,[50.92,49.08], [1,22,484,11132,256036,6182818]) # 149264638
+tests['connect4'] = (10000,21.31,[55.72,44.28], [1,7,49,343,2401,16807]) # 117649 823536 5673234
+tests['knightthrough'] = (10000,33.64,[51.67,48.33], [1,40,1600,63520,2521306,99598454]) # 3929482778
+tests['ticTacToe'] = (100000,7.63,[64.84,35.16], [1,9,72,504,3024,15120,54720]) # 148176 200448 127872
 
-tests['hex2.rbg'] = (1000,3.50,[50.00,50.00], [1,4,12,24,12,0])
 
-tests['hex9.rbg'] = (1000,71.02,[53.03,46.97], [1,81,6480]) # 511920 39929760
-
-tests['knightthrough.hrg'] = (10000,33.64,[51.67,48.33], [1,40,1600,63520,2521306,99598454]) # 3929482778
-
-tests['amazons.hrg'] = (200,71.46,[50.10,49.90], [1,2176]) # 4307152
-
-tests['repeatTest.rg'] = (100,1.0,[100.0], [1,1,0])
-tests['repeatTestBig.rg'] = (1000,1.0,[100.0], [1,2,0])
-tests['repeatTestHard.rg'] = (1000,1.0,[12.5], [1,16,0])
-tests['simpleApplyTest0.rg'] = (1000,1.0,[0.0], [1,2,0])
-tests['simpleApplyTest1.rg'] = (1000,2.0,[0.0,50.0], [1,2,4,0])
-tests['simpleApplyTest2.rg'] = (1000,2.0,[0.0,100.0], [1,2,2,0])
-tests['simpleApplyTest3.rg'] = (1000,2.0,[0.0,75.0], [1,2,3,0])
-tests['simpleApplyTest4.rg'] = (1000,2.0,[0.0,50.0], [1,1,3,0])
-tests['simpleApplyTest5.rg'] = (1000,1.0,[50.0], [1,2,0])
-tests['simpleApplyTest6.rg'] = (1000,2.0,[0.0,50.0], [1,3,5,0])
+tests['hex2'] = (1000,3.50,[50.00,50.00], [1,4,12,24,12,0])
+tests['hex9'] = (1000,71.02,[53.03,46.97], [1,81,6480]) # 511920 39929760
 
 
 if "all" in games:
   games = []
-  games.append('ticTacToe.rg')
-  games.append('breakthrough.rg')
-
   games.append('repeatTest.rg')
   games.append('repeatTestBig.rg')
   games.append('repeatTestHard.rg')
@@ -79,15 +70,23 @@ if "all" in games:
   games.append('simpleApplyTest5.rg')
   games.append('simpleApplyTest6.rg')
 
-  #games.append('ticTacToe.rbg')
-  #games.append('breakthrough.hrg')
-  #games.append('breakthrough.rbg')
-  #games.append('connect4.hrg')
-  #games.append('hex2.rbg')
-  #games.append('hex9.rbg')
-  #games.append('knightthrough.hrg')
-  #games.append('amazons-smart.hrg')
-  #games.append('amazons-naive.hrg')
+  games.append('ticTacToe.rg')
+  games.append('ticTacToe.hrg')
+  games.append('ticTacToe.rbg')
+  
+  games.append('breakthrough.rg')
+  games.append('breakthrough-simple.rg')
+  games.append('breakthrough.hrg')
+  games.append('breakthrough.rbg')
+  
+  games.append('connect4.hrg')
+  games.append('connect4.rbg')
+  
+  games.append('knightthrough.hrg')
+  games.append('knightthrough.rbg')
+  
+  games.append('amazons.hrg')
+  games.append('amazons.rbg')
 
 print(f'Testing #{len(games)}: {" ".join(games)}')
 print(f'Translate options: {translateOptions}')
@@ -117,16 +116,14 @@ totalStartTime = time.time()
 for game in games:
   print()
 
-  nameExt = game.split('.')
-  baseName = game.split('-')[0]
-  gameFile = nameExt[1] + '/' + game
-  if game in tests:
-    gameRef = game
+  nameWithExt = game.split('.')
+  baseName = nameWithExt[0].split('-')[0]
+  gameFile = nameWithExt[1] + '/' + game
+  if baseName in tests:
+    gameRef = baseName
   else:
-    gameRef = baseName + '.' + nameExt[1]
-    if gameRef not in tests:
-      print(f'Not matched tests for game {game}')
-      continue
+    print(f'No tests for game {game}')
+    continue
 
   ######## Compile ########
   print(HEAD_FORMATTER.format(f'{game} compile:'),end='',flush=True)

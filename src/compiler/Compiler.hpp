8b809edc581a40c1f483105c57dc74cbbe61f373
WoojtekP <pawlik.wj@gmail.com>
2025-03-04 22:05:34 +0100

    Use unique names of temporary variables for AssignmentAny

--- src/compiler/Compiler.hpp
+++ src/compiler/Compiler.hpp
@@ -129,7 +129,9 @@ private:
     std::unique_ptr<IValue> generateMapValue(const nlohmann::json &value);
     std::unique_ptr<IInstruction> debugInstruction(std::string functionName);
     std::unique_ptr<BlockInstruction> wrapIntoLoopIfNeeded(
-        const std::shared_ptr<IAction>& actionAssignAny, std::unique_ptr<BlockInstruction> blockInstruction) const;
+        const std::shared_ptr<IAction>& actionAssignAny,
+        std::unique_ptr<BlockInstruction> blockInstruction,
+        const std::string& tmpVariableName) const;
     std::string getTemporaryVariableName(int idx, int edgeId);
     int getNumberOfPlayers();
     void initializePragmaVerticesSet(const std::string &pragmaName, std::set<std::string> &data);

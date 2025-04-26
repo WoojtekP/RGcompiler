a3bee4618b6b4b2f1704a2b44f90694f916f2911
MSz <marek.esz@gmail.com>
2025-04-26 19:44:43 +0200

    Update option list

--- scripts/common.py
+++ scripts/common.py
@@ -39,8 +39,9 @@ class cfg:
 
   DEFAULT_TRANSLATE_OPTIONS = '--enable-all-optimizations --enable-all-pragmas'
   #DEFAULT_TRANSLATE_OPTIONS = '--enable-all-optimizations --calculate-disjoints --calculate-repeats-and-uniques --calculate-tag-indexes' # No simpleApply
+  #DEFAULT_TRANSLATE_OPTIONS = '--enable-all-optimizations --calculate-repeats-and-uniques --calculate-tag-indexes' # No simpleApply
   #DEFAULT_TRANSLATE_OPTIONS = ' --enable-all-pragmas'
-  #DEFAULT_TRANSLATE_OPTIONS += ' --compact-comparisons --compact-skip-edges --inline-assignment --inline-reachability --join-exclusive-edges --join-fork-prefixes --join-fork-suffixes --merge-accesses --propagate-constants --prune-singleton-types --prune-unreachable-nodes --prune-unused-constants --prune-unused-variables --skip-artificial-tags --skip-self-assignments --skip-self-comparisons --skip-unused-tags'
+  #DEFAULT_TRANSLATE_OPTIONS += ' --compact-comparisons --compact-skip-edges --inline-assignment --inline-reachability --join-exclusive-edges --join-fork-prefixes --join-fork-suffixes --merge-accesses --propagate-constants --prune-singleton-types --prune-unreachable-nodes --prune-unused-constants --prune-unused-variables --skip-artificial-tags --skip-redundant-tags --skip-self-assignments --skip-self-comparisons --skip-unused-tags'
   DEFAULT_RG2CPP_OPTIONS = '--simple-path 0 --disjoint 1 --all-unique 0 --max-move-len -1'
   DEBUG_RG2CPP_OPTIONS = '--no-cycle-detection 0 --print-function-names 0 --preserve-original-node-names 1 --verification 0 --gccinline 0'
 

1b09bd51606f8630e88d86ae2061b8826506977e
Lukasz Galas <galaslukasz1998@gmail.com>
2025-05-25 22:50:47 +0200

    Added common/Common.hpp file.

---
+++ src/common/Common.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <memory>
+#include <string_view>
+
+#include <graph/Action.hpp>
+namespace common
+{
+constexpr std::string_view playerWord = "player";
+constexpr std::string_view keeperWord = "keeper";
+constexpr std::string_view keeperCastedWord = "static_cast<PlayerOrSystem>(keeper";
+
+bool isActionAssigmentToPlayer(const std::shared_ptr<IAction>& action)
+{
+    return action->getType() == ActionType::Assignment && action->getLeftSide() == playerWord;
+}
+
+bool isActionAssigmentKeeperToPlayer(const std::shared_ptr<IAction>& action)
+{
+    return action->getType() == ActionType::Assignment && action->getLeftSide() == playerWord &&
+           (action->getRightSide() == keeperCastedWord || action->getRightSide() == keeperWord);
+}
+
+}  // namespace common

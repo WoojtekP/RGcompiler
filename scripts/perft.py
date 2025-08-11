0b04d206c55bcc49942af8abca53d87b46b33c26
MSz <marek.esz@gmail.com>
2025-08-11 18:44:53 +0200

    Upgrade perft and remove old compilation script

--- scripts/perft.py
+++ scripts/perft.py
@@ -6,63 +6,101 @@ os.chdir(os.path.dirname(sys.argv[0])+"/..") # RGCompiler dir
 parser = argparse.ArgumentParser(description='Compile and run perft.')
 parser.add_argument('game', nargs=1, help='game file')
 parser.add_argument('depth', nargs=1, help='perft depth')
-parser.add_argument('-t', dest='translateOptions', nargs='?', help='translate options for interpreter_node/lib/cli', default=cfg.DEFAULT_TRANSLATE_OPTIONS)
-parser.add_argument('-skipcompilation', action='store_true', help='skip compile.py and use the existing reasoner sources')
+parser.add_argument('-t', dest='translateFlags', nargs='?', help='translate flags for interpreter_rust', default=cfg.DEFAULT_TRANSLATE_OPTIONS)
+parser.add_argument('--reuse', action='store_true', help='reuse already built reasoner (no translation nor rg2cpp)')
+parser.add_argument('--perf', action='store_true', help='count instructions by perf')
+parser.add_argument('--clang', action='store_true', help='use clang++ instead of g++')
 cpp_flags_group = parser.add_mutually_exclusive_group(required=False)
-cpp_flags_group.add_argument('-debug', action='store_true', help='compile with gdb symbols')
-cpp_flags_group.add_argument('-profile', action='store_true', help='generate profiler information')
-cpp_flags_group.add_argument('-benchmark', action='store_true', help='maximum speed flags')
+cpp_flags_group.add_argument('--benchmark', action='store_true', help='maximum speed flags (default)')
+cpp_flags_group.add_argument('--test', action='store_true', help='optimization and asserts')
+cpp_flags_group.add_argument('--debug', action='store_true', help='gdb symbols and sanitizers')
+cpp_flags_group.add_argument('--profile', action='store_true', help='generate profiler information')
 
 args = parser.parse_args()
 game = args.game[0]
 depth = args.depth[0]
-translateOptions = args.translateOptions
-if args.profile:
-  gccOptions = cfg.GCC_PROFILE_FLAGS
-  infoGccOptions = "profile"
-elif args.debug:
-  gccOptions = cfg.GCC_DEBUG_FLAGS
-  infoGccOptions = "debug"
-elif args.benchmark:
-  gccOptions = cfg.GCC_BENCHMARK_FLAGS
-  infoGccOptions = "benchmark"
-else:
-  gccOptions = cfg.GCC_TEST_FLAGS
-  infoGccOptions = "test"
 
-print(f'Testing: {game}')
-print(f'Translate options: {translateOptions}')
-print(f'rg2cpp options: {cfg.DEFAULT_RG2CPP_OPTIONS}')
-print(f'g++ {infoGccOptions} options: {gccOptions}')
-#if usePerf: print(f'Using perf')
+translateFlags = args.translateFlags
+benchmarkMode = False
+if args.profile:
+  cppFlags = cfg.GCC_PROFILE_FLAGS
+  mode = "profile"
+elif args.debug:
+  cppFlags = cfg.GCC_DEBUG_FLAGS
+  mode = "debug"
+elif args.test:
+  cppFlags = cfg.GCC_TEST_FLAGS
+  mode = "test"
+else:
+  cppFlags = cfg.GCC_BENCHMARK_FLAGS
+  mode = "benchmark"
+  benchmarkMode = True
+
+if args.clang: compiler = 'clang++'
+else: compiler = 'g++'
+
+print(f'Mode {util.GREEN}{mode}{util.RESET}, limit: ',end='')
+print(f'Depth {depth}')
+print(f'Game: {game}')
+print(f'Translate flags: {translateFlags}')
+print(f'rg2cpp flags: {cfg.DEFAULT_RG2CPP_OPTIONS}')
+print(f'{compiler} flags: {cppFlags}')
+print()
 
 #######################################################################################################################
-
-if not args.skipcompilation:
-  run(f'python3 scripts/compile.py {game} -t"{translateOptions}" -silent')
 
 HEAD_FORMATTER = '{: <14} '
 TIME_FORMATTER = '{:9.3f} s'
 FULL_FORMATTER = HEAD_FORMATTER + TIME_FORMATTER
 
-startTime = time.time()
-run(f'g++ test/perft.cpp {cfg.BUILD_TEST_DIR}/reasoner.cpp -I{cfg.BUILD_TEST_DIR} {gccOptions} -o {cfg.BUILD_TEST_DIR}/perft')
-elapsedTime = time.time() - startTime
-print(FULL_FORMATTER.format('g++:',elapsedTime))
+while True:
+  print(HEAD_FORMATTER.format(f'{game}:'),end='',flush=True)
+  
+  ######## Translate ########
+  if not args.reuse:
+    print(f' | ast ',end='',flush=True)
+    startTime = time.time()
+    error = createAST(game, translateFlags, True)
+    if error != None:
+      print(f'\n{util.ERROR} {util.CYAN}{error}{util.RESET}')
+      break
+    elapsedTime = time.time() - startTime
+    print(TIME_FORMATTER.format(elapsedTime),end='',flush=True)
+    
+    print(f' | rg2cpp ',end='',flush=True)
+    startTime = time.time()
+    result = rg2cpp(game, cfg.DEFAULT_RG2CPP_OPTIONS + ' ' + cfg.DEBUG_RG2CPP_OPTIONS, True)
+    if error != None:
+      print(f'\n{util.ERROR} {util.CYAN}{error}{util.RESET}')
+      break
+    print(TIME_FORMATTER.format(elapsedTime),end='',flush=True)
+      
+  ######## Compile cpp ########
+  print(f' | {compiler} ',end='',flush=True)
+  startTime = time.time()
+  error = compileCpp('perft', compiler, f'{cppFlags}')
+  elapsedTime = time.time() - startTime
+  if error != None:
+    print(f'{util.ERROR} {util.CYAN}{error}{util.RESET}')
+    break
+  print(TIME_FORMATTER.format(elapsedTime),end='',flush=True)
 
-print(HEAD_FORMATTER.format(f'perft {str(depth)}:'),end='',flush=True)
-startTime = time.time()
-result = runCap(f'{cfg.BUILD_TEST_DIR}/perft {str(depth)}')
-elapsedTime = time.time() - startTime
-print(TIME_FORMATTER.format(elapsedTime))
+  ######## Perft ########
+  print(HEAD_FORMATTER.format(f' | perft {str(depth)}'),end='',flush=True)
+  startTime = time.time()
+  result = runCap(f'{cfg.BUILD_TEST_DIR}/perft {str(depth)}')
+  elapsedTime = time.time() - startTime
+  print(TIME_FORMATTER.format(elapsedTime))
 
-if result.returncode != 0:
-  print(f'{util.ERROR} {util.CYAN}(exitcode {result.returncode}) {decodeOutput(result.stderr)}{util.RESET}')
-  exit(2)
-stats = decodeOutput(result.stdout).strip().split(' ')
-resLeaves = int(stats[0])
-resStates = int(stats[1])
-resTerminals = int(stats[2])
-print(f'Leaves: {resLeaves}')
-print(f'States: {resStates}  ({resStates/elapsedTime:9,.0f} states/s)')
-print(f'Terminals: {resTerminals}')
+  if result.returncode != 0:
+    print(f'{util.ERROR} {util.CYAN}(exitcode {result.returncode}) {decodeOutput(result.stderr)}{util.RESET}')
+    exit(2)
+  stats = decodeOutput(result.stdout).strip().split(' ')
+  resLeaves = int(stats[0])
+  resStates = int(stats[1])
+  resTerminals = int(stats[2])
+  print(f'Leaves: {resLeaves}')
+  print(f'States: {resStates}  ({resStates/elapsedTime:9,.0f} states/s)')
+  print(f'Terminals: {resTerminals}')
+  
+  break

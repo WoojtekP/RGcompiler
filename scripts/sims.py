7c84d947ae5b5ad261d7a6b5511293b8038f8c06
Lukasz Galas <galaslukasz1998@gmail.com>
2025-08-18 22:24:37 +0200

    change in sims.py

--- scripts/sims.py
+++ scripts/sims.py
@@ -142,14 +142,13 @@ elif "reasoners" in games:
   games = []
   games.append('amazons_split2.hrg')
   games.append('gomoku_standard.hrg')
-  games.append('hex.rbg')
-  games.append('ticTacToe.rg')
-  games.append('pentago.hrg')
   games.append('breakthrough.hrg')
   games.append('connect4.hrg')
-  games.append('englishDraughts.hrg')
-  games.append('reversi.rbg')
   games.append('chess.hrg')
+  games.append('englishDraughts.hrg')
+  games.append('hex.hrg')
+  games.append('pentago.hrg')
+  games.append('reversi.hrg')
 
 print(f'Mode {util.GREEN}{mode}{util.RESET}, limit: ',end='')
 if useTime: print(f'{limitS}s')
